2d4e580562f92d48ad6fb1e1d94976356eb188f4
Konstantin <growingentity@gmail.com>
2024-12-06 09:21:50 -0800

    fix

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      "grizzly-tattoo.puresitestudio.com",
       "lh3.googleusercontent.com",
       "backend.grizzlytattoo.com",
+      "grizzly-tattoo.puresitestudio.com",
     ],
   },
 
@@ -19,6 +19,39 @@ const nextConfig = {
       {
         source: "/Contact",
         destination: "/contact-us",
+        permanent: true,
+      },
+      {
+        source: "/tyler-adams-tattoo-artist",
+        destination: "/artists/tyler-adams",
+        permanent: true,
+      },
+      {
+        source: "/brian-visser",
+        destination: "/artists/brian-visser",
+        permanent: true,
+      },
+      {
+        source: "/blane",
+        destination: "/artists/blane-curtice",
+        permanent: true,
+      },
+
+      {
+        source: "/blane-1",
+        destination: "/artists/leo-salazar",
+        permanent: true,
+      },
+
+      {
+        source: "/derek-yost",
+        destination: "/artists/derek-yost",
+        permanent: true,
+      },
+
+      {
+        source: "/portland-tattoo-artists/tyler-adams/",
+        destination: "/artists/tyler-adams",
         permanent: true,
       },
     ];
