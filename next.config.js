167776f5e5a983d6b870301fca5d71f135b508ac
Konstantin <growingentity@gmail.com>
2024-12-06 09:03:31 -0800

    wip

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["grizzly-tattoo.puresitestudio.com", "lh3.googleusercontent.com"],
+    domains: [
+      "grizzly-tattoo.puresitestudio.com",
+      "lh3.googleusercontent.com",
+      "backend.grizzlytattoo.com",
+    ],
   },
 
   async redirects() {
