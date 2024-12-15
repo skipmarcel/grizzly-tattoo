a913d28c863cac33d440003aa2446d2c209d29f2
Tyler Adams <skipmarcel@gmail.com>
2024-12-15 09:44:26 -0800

    minus white space from booking

--- sections/bookingPage/BookingForm.tsx
+++ sections/bookingPage/BookingForm.tsx
@@ -12,17 +12,9 @@ const BookingForm = ({
   artist?: any;
 }) => {
   return (
-    <section className="pt-28 lg:pt-40">
+    <section className="pt-10 lg:pt-20">
       <div className="container relative">
-        {/* <Image
-          src="/images/booking/svg.png"
-          alt={`Make a Reservation`}
-          width={500}
-          height={500}
-          className="!absolute object-cover top-0 -translate-y-full left-1/2 -translate-x-1/2 max-xs:w-[90%] max-md:max-w-xs"
-        /> */}
-
-        <div className="py-10 px-6 lg:p-10 border border-gray-100 shadow-lg bg-black text-white max-w-5xl mx-auto">
+        <div className="py-10 px-6 lg:p-10 border border-gray-100 shadow-lg bg-black text-white max-w-5xl mx-auto rounded-lg">
           {!artistSlug ? (
             <ChooseArtist {...{ artists }} />
           ) : (
